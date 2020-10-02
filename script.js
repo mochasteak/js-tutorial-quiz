71475272dca4424ed42e71338b00c0d1c87ddaf1
Brian Bishop <mochasteak@gmail.com>
2020-10-02 18:33:59 +0100

    First commit, HTML template, CSS, and JS outline

---
+++ script.js
@@ -0,0 +1,16 @@
+const startButton = document.getElementById('start-btn');
+
+startButton.addEventListener('click', startGame);
+
+
+function startGame() {
+  console.log('starting game');
+}
+
+function setNextQuestion() {
+
+}
+
+function selectAnswer() {
+
+}
